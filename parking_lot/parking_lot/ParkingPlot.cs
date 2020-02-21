5344cb09634aa1960bf4381eb9ce48d5e91f63a9
Angel <Angel@DESKTOP-644D4F0>
2020-02-21 21:26:14 +0530

    update adding class for parking

---
+++ parking_lot/parking_lot/ParkingPlot.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace parking_lot
+{
+
+
+
+    public class ParkingPlot
+    {
+        const int AvailablePlotSize = 6;
+        List<string> Park;
+        public ParkingPlot()
+        {
+            var Park = new List<string>()
+            {
+                "KA-01-HH-1234",
+                "KA-01-HH-9999",
+                "KA-01-BB-0001",
+                "KA-01-HH-7777",
+                "KA-01-HH-2701",
+                "KA-01-HH-3141"
+            };
+        }
+
+        public bool CheckParkingIsAvailable()
+        {
+            return Park.Count < (AvailablePlotSize - 1);
+        }
+
+        public void Leave(string CarNumber)
+        {
+            Park.Remove(CarNumber);
+        }
+        public void Book(string CarNumber)
+        {
+            Park.Add(CarNumber);
+        }
+        public IEnumerable<string> Status()
+        {
+            return Park;
+        }
+
+
+    }
+}
--- parking_lot/parking_lot/Program.cs
+++ parking_lot/parking_lot/Program.cs
@@ -4,9 +4,69 @@ namespace parking_lot
 {
     class Program
     {
+        public string[] InputData()
+        {
+            return @"create_parking_lot 6 
+park KA-01-HH-1234 
+park KA-01-HH-9999 
+park KA-01-BB-0001 
+park KA-01-HH-7777 
+park KA-01-HH-2701 
+park KA-01-HH-3141 
+leave KA-01-HH-3141 4 
+status 
+park KA-01-P-333 
+park DL-12-AA-9999 
+leave KA-01-HH-1234 4 
+leave KA-01-BB-0001 6 
+leave DL-12-AA-9999 2 
+park KA-09-HH-0987 
+park CA-09-IO-1111 
+park KA-09-HH-0123 
+status".Split('\n',StringSplitOptions.RemoveEmptyEntries);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ParkingPlot parkingPlot = new ParkingPlot();
+            while (true)
+            {
+                switch (UserInput())
+                {
+                    case "1":
+                        Console.WriteLine("Enter Car Number:...  ");
+                        var carNumber_Book = Console.ReadLine();
+                        parkingPlot.Book(carNumber_Book);
+                        break;
+                    case "2":
+                        Console.WriteLine("Enter Car Number:...  ");
+                        var carNumber_Leave = Console.ReadLine();
+                        parkingPlot.Book(carNumber_Leave);
+                        break;
+                    case "3":
+                        var cars = parkingPlot.Status();
+                        foreach (var car in cars)
+                        {
+                            Console.WriteLine(car)
+                        }
+                        break;
+                    case "4":
+                        return;
+                    default:
+                        Console.WriteLine("Wrong input...! Please select option from the list.");
+                        break;
+                }
+            }
+        }
+
+        public static string UserInput()
+        {
+            Console.WriteLine("Enter you choise...");
+            Console.WriteLine("1. Book Parking");
+            Console.WriteLine("2. Leave Parking");
+            Console.WriteLine("3. Parking Status");
+            Console.WriteLine("4. Exit");
+            return Console.ReadLine();
         }
     }
 }
